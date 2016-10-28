4a14548a4d24ccd2282a19082d86f835e4f128f0
Joost de Locht <joost@echodes.nl>
2016-10-28 14:57:05 +0200

    connects PdoRep to database

--- application/PdoSessionRepository.php
+++ application/PdoSessionRepository.php
@@ -2,64 +2,32 @@
 
 class PdoSessionRepository implements SessionRepository
 {
-
-   public function __construct()
-    {
-    }
+    /**
+     * @var PDO
+     */
+    private $link;
 
     /**
-     * @param $link
-     * @return SessionResult[]
-     * @internal param $session
+     * PdoSessionRepository constructor.
+     * @param PDO $link
      */
-    public function getResultsForSession($link)
+    public function __construct(PDO $link)
     {
-        $session = new PdoSessionRepository($link);
-//        $session = 'SELECT
-//              `driver`,
-//              `number`,
-//              `nationality`,
-//              `team`,
-//              `grandprix`,
-//              `sessionone`,
-//              `sessiontwo`,
-//              `sessionthree`,
-//              `sessionfour`,
-//              `sessionfive`
-//              FROM `formulaone`';
-//
-//        $sessionResults = [];
-//        foreach ($link->query($session) as $row) {
-//            $sessionResults[] = $row['driver'] . $row['number'] . $row['nationality'] . $row['team'] . $row['grandprix'] . $row['sessionone'] . $row['sessiontwo'] . $row['sessionthree'] . $row['sessionfour'] . $row['sessionfive'];
-//        }
-//        echo $sessionResults;
+        $this->link = $link;
     }
 
-
-//
-    // Doe een query om een array met de goede data te krijgen
-
-    // Zet de array van data om naar results
-//        $results = [];
-//        foreach ($goedeData as $regelUitDeDatabase) {
-//            $results[] =    -('..', '..', 94873465, 4);
-//        }
-//        return $results;
-    /**
-     * @param $link
-     * @return string
-     */
-    public function getResultsForQualifying($link)
+    public function getResultsForSession($session)
     {
-        return '';
+        $this->link->query($session);
     }
 
-    /**
-     * @param $link
-     * @return string
-     */
-    public function getResultsForRace($link)
+    public function getResultsForQualifying($qualifying)
     {
-        return '';
+        // TODO: Implement getResultsForQualifying() method.
+    }
+
+    public function getResultsForRace($race)
+    {
+        // TODO: Implement getResultsForRace() method.
     }
 }
