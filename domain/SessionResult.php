381cce2b5c4f4caca3d5af4da67a27e5b1674d5b
Joost de Locht <joost@echodes.nl>
2016-11-03 10:10:24 +0100

    moves Calculator method from SessionResult to ResultLine

--- domain/SessionResult.php
+++ domain/SessionResult.php
@@ -28,18 +28,4 @@ class SessionResult
     {
         return $this->resultLines;
     }
-
-    /**
-     * @param $lapTime
-     * @param $slowerLapTime
-     * @return mixed
-     */
-    public function calculateLapTimeDifferenceBetween($lapTime, $slowerLapTime)
-    {
-        $difference = floor(($slowerLapTime - $lapTime) * 1000);
-        $differenceInSeconds = floor($difference / 1000);
-        $differenceInMilliseconds = floor($difference % 1000);
-
-        return '+'  . $differenceInSeconds . '.' . str_pad($differenceInMilliseconds, 3, '0', STR_PAD_LEFT);
-    }
 }
